e5e996c44387f0003699e445b3e7b3e3326b3323
vijaydass <bfirst176@gmail.com>
2025-03-12 20:34:04 +0530

    logo changed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
         <a class="navbar-brand brand-logo" href="{{route('dashboard')}}"><img src="{{asset('assets/images/vyom.png')}}" alt="logo" /></a>
-        <a class="navbar-brand brand-logo-mini" href="{{route('dashboard')}}"><img src="{{asset('assets/images/vyom.png')}}"
+        <a class="navbar-brand brand-logo-mini" href="{{route('dashboard')}}"><img src="{{asset('assets/images/vyom-mini.png')}}"
                 alt="logo" /></a>
     </div>
     <div class="navbar-menu-wrapper d-flex align-items-stretch">
